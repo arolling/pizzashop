956c168d763d5c11e81738581a52adf088122b87
Abigail Rolling <arolling@gmail.com>
2016-01-29 09:02:50 -0800

    create Pizza prototype method to choose size and set price

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-var Pizza = function(kind, basePrices){
+function Pizza(kind, basePrices){
   this.kind = kind;
+  this.pieSize = '';
   this.basePrices = basePrices; // an array of the small, medium, large prices for this topping
   this.extras = []; // names extra toppings, modifications (ie gluten free)
   this.total = 0; // as extras are added, their cost adds to total
+}
+
+Pizza.prototype.setSize = function(size){
+  this.pieSize = size;
+  if (this.pieSize === "Small"){
+    this.total += this.basePrices[0];
+  } else if (this.pieSize === "Medium"){
+    this.total += this.basePrices[1];
+  } else if (this.pieSize === "Large"){
+    this.total += this.basePrices[2];
+  } else {
+    alert("Not a valid size");
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -5,5 +5,14 @@ describe ('Pizza', function (){
     expect(testPizza.extras).to.eql([]);
     expect(testPizza.basePrices).to.eql([599, 799, 999]);
     expect(testPizza.total).to.eql(0);
+    console.log(testPizza);
+  });
+
+  it('will select the size of the pizza and add the price to the total cost', function() {
+    var testPizza = new Pizza("Pepperoni", [599, 799, 999]);
+    testPizza.setSize('Small');
+    expect(testPizza.pieSize).to.equal('Small');
+    expect(testPizza.total).to.equal(599);
+    console.log(testPizza);
   });
 });
